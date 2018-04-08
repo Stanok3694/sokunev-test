8919690188fc206d6579a5f4a2ff2df89c16c05d
Stanislav Okunev <stanok3694@yandex.ru>
2018-04-08 16:32:29 +0300

    mocks: updated mock content (test preparations)

--- src/mocks/MockData.js
+++ src/mocks/MockData.js
@@ -3,37 +3,37 @@ import uuidv1 from "uuid/v1";
 const MockData = [
     {
         id: uuidv1(),
-        name: "blabla",
-        surname: "fafa",
+        name: "name-1",
+        surname: "surname-1",
         subchilds: [
             {
                 id: uuidv1(),
-                name: "blabla",
-                surname: "fafa",
+                name: "name-2",
+                surname: "surname-2",
                 subchilds: [
                     {
                         id: uuidv1(),
-                        name: "blabla",
-                        surname: "fafa",
+                        name: "name-3",
+                        surname: "surname-3",
                     }
                 ]
             },
             {
                 id: uuidv1(),
-                name: "blabla",
-                surname: "fafa",
+                name: "name-4",
+                surname: "surname-4",
             },
         ]
     },
     {
         id: uuidv1(),
-        name: "blabla",
-        surname: "fafa"
+        name: "name-5",
+        surname: "surname-5"
     },
     {
         id: uuidv1(),
-        name: "blabla",
-        surname: "fafa",
+        name: "name-6",
+        surname: "surname-6",
     }
 ];
 
