1dc29ee2fd67546b7be6a506429ada0e57aa82b0
Stanislav Okunev <stanok3694@yandex.ru>
2018-04-08 18:00:08 +0300

    mock: updated mocks + test

--- src/__tests__/EditTreeService.test.js
+++ src/__tests__/EditTreeService.test.js
@@ -1,12 +1,12 @@
-import { EditTreeService } from "../services";
+import { EditTreeService, CreateMockedElement } from "../services";
 import { MockData } from "../mocks";
 
 const initialData = MockData;
-// const newElem = AddNewElem(initialData);
+const newElem = CreateMockedElement(initialData);
 
 describe('EditTree: ', () => {
     it.skip('should add new elem or child', () => {
-        //0
+        console.log(initialData);
     });
     it.skip('should delete elem or child by id', () => {
         //
--- src/mocks/MockData.js
+++ src/mocks/MockData.js
@@ -8,32 +8,32 @@ const MockData = [
         subchilds: [
             {
                 id: uuidv1(),
-                name: "name-2",
-                surname: "surname-2",
+                name: "name-1",
+                surname: "surname-1",
                 subchilds: [
                     {
                         id: uuidv1(),
-                        name: "name-3",
-                        surname: "surname-3",
+                        name: "name-1",
+                        surname: "surname-1",
                     }
                 ]
             },
             {
                 id: uuidv1(),
-                name: "name-4",
-                surname: "surname-4",
+                name: "name-2",
+                surname: "surname-2",
             },
         ]
     },
     {
         id: uuidv1(),
-        name: "name-5",
-        surname: "surname-5"
+        name: "name-2",
+        surname: "surname-2"
     },
     {
         id: uuidv1(),
-        name: "name-6",
-        surname: "surname-6",
+        name: "name-3",
+        surname: "surname-3",
     }
 ];
 
